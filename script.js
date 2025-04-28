668646aa5dd4fce69b65be5a54be0bb039d9b6c9
Erick Oliveira <ericksoliveira69@gmail.com>
2025-04-28 15:19:39 -0300

    feat: carrossel implementado e atualizado

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 const sliderTabs = document.querySelectorAll(".slider-tab");
 const sliderIndicator = document.querySelector(".slider-indicator");
+const sliderControls = document.querySelector(".slider-controls");
 
 
 // Update the indicator height and width
-const updatePagination = (tab, index) => {
+const updateIndicator = (tab, index) => {
     sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
     sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+
+    // Calculate the scroll position and scroll smoothly
+    const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+    sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth"});
 }
 
 // Initialize swiper instance
 const swiper = new Swiper(".slider-container", {
     effect: "slide",
     speed: 1300,
-    autoplay: { delay: 4000 }
+    autoplay: { delay: 4000 },
+    navigation: {
+        prevEl: "#slide-prev",
+        nextEl: "#slide-next",
+    },
+    on: {
+        // Update indicator on slide change
+        slideChange: () => {
+            const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
+            updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+        }
+    },
+    reachEnd: () => swiper.autoplay.stop()
 });
 
 // Update the slide on and pagination on tab click
 sliderTabs.forEach((tab, index) => {
     tab.addEventListener("click", () => {
         swiper.slideTo(index);
-        updatePagination(tab, index);
+        updateIndicator(tab, index);
     });
 });
 
-updatePagination(sliderTabs[0], 0);
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("resize", () => updatePagination(sliderTabs[swiper.activeIndex], 0));
